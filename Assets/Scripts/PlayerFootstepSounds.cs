f17dfdb3dae5e7c1ffa0863eee5e753f2e79a956
ergav <erik.gafvelin@adelfors.nu>
2022-03-10 11:34:55 +0100

    Worked on footstep script

--- Assets/Scripts/PlayerFootstepSounds.cs
+++ Assets/Scripts/PlayerFootstepSounds.cs
@@ -6,9 +6,13 @@ public class PlayerFootstepSounds : MonoBehaviour
 {
     PlayerController playerController;
     AudioSource audioSource;
-    [SerializeField] AudioSource[] metalSounds, woodSounds, carpetSounds;
+    [SerializeField] AudioClip[] metalSounds, woodSounds, carpetSounds;
     [SerializeField] float walkSoundRate = 0.75f, runSoundRate = 0.25f;
+    float soundRate;
     int rng;
+
+    public bool moving;
+    public bool sprinting;
 
     void Start()
     {
@@ -16,8 +20,38 @@ public class PlayerFootstepSounds : MonoBehaviour
         audioSource = GetComponentInChildren<AudioSource>();
     }
 
+    float timer;
     void Update()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 3))
+        {
+
+        }
+
+        if (moving)
+        {
+            timer += Time.deltaTime;
+            if (timer > soundRate)
+            {
+                rng = Random.Range(0, woodSounds.Length);
+                audioSource.PlayOneShot(woodSounds[rng]);
+                timer = 0;
+            }
+        }
+
+        if (sprinting)
+        {
+            soundRate = runSoundRate;
+        }
+        else
+        {
+            soundRate = walkSoundRate;
+        }
+
+
     }
+
+
+
 }
