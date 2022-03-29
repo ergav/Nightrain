b8f8fb2460ecbd2ef0f21988a856eb71d49a8618
GrDelta <ricardo.svensson17@gmail.com>
2022-03-29 10:50:18 +0200

    Door function.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,33 +4,59 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    InputManager inputManager;
     PlayerController player;
     Animator ator;
 
-    private bool doorIsOpen = false;
+    private bool doorIsClosed = true;
+    private bool doorCanBeOpen = false;
     private void Start()
     {
+        inputManager = InputManager.Instance;
         player = FindObjectOfType<PlayerController>();
         ator = GetComponent<Animator>();
     }
+
+    private void Update()
+    {
+        DoorInteraction();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !doorIsOpen)
+        if (other.CompareTag("Player"))
         {
-            ator.Play("Door_Open");
-            doorIsOpen = true;
-            
+            Debug.Log("Door is ready to be open");
+            doorCanBeOpen = true;
+            doorIsClosed = true;
         }
+
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player") && doorIsOpen)
+        if (other.CompareTag("Player"))
         {
-            ator.Play("Door_Close");
-            doorIsOpen = false;
-
+            Debug.Log("player left the are to open door");
+            doorIsClosed = false;
+            doorCanBeOpen = false;
         }
     }
 
+    private void DoorInteraction()
+    {
+        if (doorIsClosed && doorCanBeOpen && inputManager.ActionInteract())
+        {
+            doorIsClosed = false;
+            ator.SetBool("OpenDoor", true);
+            ator.SetBool("CloseDoor", false);
+
+        }
+        else if (!doorIsClosed && doorCanBeOpen && inputManager.ActionInteract())
+        {
+            doorIsClosed = true;
+            ator.SetBool("OpenDoor", false);
+            ator.SetBool("CloseDoor", true);
+        }
+    }
 }
