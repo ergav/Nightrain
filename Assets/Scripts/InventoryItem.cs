7396a63624d13a61e8237029ec6099e8975061b5
ergav <erik.gafvelin@adelfors.nu>
2022-04-20 13:56:35 +0200

    train updates

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : MonoBehaviour
+{
+    public int[,] slotsToOccupy = new int[1,1];
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
