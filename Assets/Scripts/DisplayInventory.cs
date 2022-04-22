a9c008ccf6f64770de0b70ad33734ce1fb50f60c
ergav <erik.gafvelin@adelfors.nu>
2022-04-22 11:37:34 +0200

    trying to work on inventory again

--- Assets/Scripts/DisplayInventory.cs
+++ Assets/Scripts/DisplayInventory.cs
@@ -7,6 +7,7 @@ public class DisplayInventory : MonoBehaviour
     Inventory inventory;
     GridLayout gridLayout;
     public inventorySlot[] inventorySlots;
+    bool fullInventory;
 
     void Start()
     {
@@ -17,5 +18,19 @@ public class DisplayInventory : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void AddItem(GameObject item)
+    {
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            if (!inventorySlots[i].hasItem)
+            {
+                GameObject addedItem = Instantiate(item);
+                addedItem.transform.SetParent(inventorySlots[i].transform);
+                addedItem.transform.position = Vector3.zero;
+                return;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/ItemTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTest : MonoBehaviour
+{
+    public GameObject inventoryObject;
+    DisplayInventory displayInventory;
+
+    void Start()
+    {
+        displayInventory = GameObject.FindObjectOfType<DisplayInventory>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            displayInventory.AddItem(inventoryObject);
+            Destroy(gameObject);
+        }
+    }
+}
